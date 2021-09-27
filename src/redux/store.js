488bdb1e0c12383a8f9a823661c819c7a90126c8
josh-frank <eurosewusa@gmail.com>
2021-09-27 16:34:21 -0400

    removed stations reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clientReducer from "./clientSlice"
 import displayReducer from "./displaySlice"
-import stationReducer from "./stationSlice"
 
 const store = configureStore( {
     reducer: {
         client: clientReducer,
         display: displayReducer,
-        stations: stationReducer,
     },
     enhancer: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 } );
